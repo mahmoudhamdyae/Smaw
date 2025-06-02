c025928b5a57a91e58f50c5e25de4fb26959d882
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2025-06-02 13:24:20 +0300

    Images

---
+++ lib/core/resources/assets_manager.dart
@@ -0,0 +1,28 @@
+const String imagePath = "assets/images";
+
+class AssetsManager {
+
+  static const String bell = "$imagePath/bell.png";
+
+  /// Avatars
+  static const String avatar1 = "$imagePath/avatar_1.png";
+  static const String avatar2 = "$imagePath/avatar_2.png";
+
+  /// Languages
+  static const String ara = "$imagePath/ara.png";
+  static const String eng = "$imagePath/eng.png";
+
+  /// Bottom Nav Bar Icons
+  static const String home = "$imagePath/home.png";
+  static const String orders = "$imagePath/orders.png";
+  static const String attendance = "$imagePath/attendance.png";
+  static const String menu = "$imagePath/menu.png";
+
+  /// Drawer
+  static const String ordersFill = "$imagePath/orders_fill.png";
+  static const String attendanceFill = "$imagePath/attendance_fill.png";
+  static const String summary = "$imagePath/summary.png";
+  static const String policy = "$imagePath/policy.png";
+  static const String about = "$imagePath/about.png";
+  static const String logOut = "$imagePath/log_out.png";
+}
